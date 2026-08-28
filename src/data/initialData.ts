4d1fd68c9c2a8e86e158a6c07eae3fc233946e1e
husnukazli <husnukazli@gmail.com>
2026-08-28 13:17:05 +0300

    Update initialData.ts

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -3,20 +3,10 @@ export const INITIAL_MATCHES: any[] = [];
 
 // Varsayılan hakemler
 export const INITIAL_REFEREES = [
-  { name: 'CANAN ÇAPLIK', pin: '1212' },
-  { name: 'FURKAN GÖK', pin: '1313' },
-  { name: 'DERİN GÜLER', pin: '1414' },
+ 
 ];
 
 // Varsayılan format hafızası
 export const INITIAL_CATEGORY_FORMAT_MEMORY: Record<string, string> = {
-  'Kadın 8 Yaş T': '2 Kısa Set, 3. Set 10 Puanlık Maç Tie-Break',
-  'Erkek 9 Yaş T': '2 Kısa Set, 3. Set 10 Puanlık Maç Tie-Break',
-  'Kadın 9 Yaş T': '2 Kısa Set, 3. Set 10 Puanlık Maç Tie-Break',
-  'Erkek 10 Yaş T': '3 Kısa Set',
-  'Kadın 10 Yaş T': '3 Kısa Set',
-  '12 Yaş Erkek': '3 Normal Set',
-  '12 Yaş Kadın': '3 Normal Set',
-  '14 Yaş': '3 Normal Set',
-  'Büyükler': '3 Normal Set',
+ 
 };
